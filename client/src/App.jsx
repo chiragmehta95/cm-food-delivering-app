d1e89c09edeea2d214cf091fe8e3d9ca7a7a5f34
chirag <chiragmehta95@gmail.com>
2026-05-15 11:12:36 +0530

    new featurew

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,39 @@
 export default function App() {
+  const foods = [
+    {
+      id: 1,
+      name: "Veg Thali",
+      price: 80
+    },
+    {
+      id: 2,
+      name: "Paneer Rice",
+      price: 120
+    }
+  ]
+
   return (
     <div style={{ padding: 40 }}>
-      <h1>Hello Food App</h1>
+      <h1>Food Ordering App</h1>
+
+      {foods.map((food) => (
+        <div
+          key={food.id}
+          style={{
+            border: "1px solid #ddd",
+            padding: 20,
+            marginTop: 20,
+            borderRadius: 10
+          }}
+        >
+          <h2>{food.name}</h2>
+          <p>₹{food.price}</p>
+
+          <button>
+            Add To Cart
+          </button>
+        </div>
+      ))}
     </div>
   )
 }
