a71988cc91990180b05304a28ceeb359c7413102
chirag <chiragmehta95@gmail.com>
2026-05-15 14:11:48 +0530

    fixes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+
 require('dotenv').config()
 
 const app = express()
@@ -8,25 +9,33 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Food Ordering API Running')
+  res.send('Backend Running')
 })
 
 app.get('/api/menu/today', (req, res) => {
   res.json([
-    { id: 1, name: 'Veg Thali', price: 80 },
-    { id: 2, name: 'Paneer Rice', price: 120 }
+    {
+      id: 1,
+      name: 'Veg Thali',
+      price: 80
+    },
+    {
+      id: 2,
+      name: 'Paneer Rice',
+      price: 120
+    },
+    {
+      id: 3,
+      name: 'Dal Rice',
+      price: 60
+    }
   ])
-})
-
-app.post('/api/orders/create', (req, res) => {
-  res.json({
-    success: true,
-    message: 'Order placed successfully'
-  })
 })
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(
+    `Server running on port ${PORT}`
+  )
 })
